6636916adccae77e9d8cef39cb39257bb3e313a8
Loïc Linder <loic.linder@epitech.eu>
2017-03-28 14:42:51 +0200

    check ftok + return main

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login   <linder@epitech.net>
 ** 
 ** Started on  Tue Mar 28 10:48:20 2017 linder
-** Last update Tue Mar 28 12:06:04 2017 linder
+** Last update Tue Mar 28 14:42:17 2017 linder
 */
 
 #include <sys/ipc.h>
@@ -15,17 +15,17 @@
 #include <string.h>
 #include "../include/lemipc.h"
 
-int	start(char *path, int team_nb)
+static void	start(char *path, int team_nb)
 {
-  key_t	key;
+  key_t		key;
 
-  key = ftok(path, 0);
+  if ((key = ftok(path, 0)) == -1)
+    exit_error("Error : Wrong path\n", 84);
   printf("key : %d\n", key);
   team_nb = team_nb;
-  exit(0);
 }
 
-void	usage()
+static void	usage()
 {
   printf("Usage : ./lemipc path_to_key team_number\n\n");
   printf("path_to_key is a valid path that will be use by ftok\n");
@@ -33,13 +33,14 @@ void	usage()
   exit(0);
 }
 
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
   if (ac == 2 && strcmp(av[1], "-h") == 0)
     usage();
   if (ac != 3)
-    exit_error("The number of arguments should be 3. Use ./lemipc -h for more details\n", 84);
+    exit_error("The number of arguments should be 3. Use \"./lemipc -h\" for more details\n", 84);
   if (!atoi(av[2]))
     exit_error("The second argument must be a number\n", 84);
   start(av[1], atoi(av[2]));
+  return (0);
 }
